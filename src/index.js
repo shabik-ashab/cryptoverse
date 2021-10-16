482fc4fc08dcaea4b40637c5ab9bb42a66f5daa6
nub69 <shabik.ashab@gmail.com>
2021-10-16 14:43:05 +0600

    top 10 crypto added

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+import store from './App/store';
+
+
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+       <Provider store={store}>
+           <App />
+       </Provider>
+    </Router>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
